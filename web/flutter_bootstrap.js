8b4b20cf885b05e6dd3a306d7089abee13a9b4eb
Moon Kang <GYNj7@KTAvb*DSm&dPb%xX9Bjmd3fiq$2@5VA9xUXVT@@p*u9yvB2*Tf45gn^ket>
2026-01-30 20:42:14 -0500

    Update frontend

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1583976080"
+    serviceWorkerVersion: "184375625"
   }
 });
