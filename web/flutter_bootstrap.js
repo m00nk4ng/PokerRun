acc08fb062d42878ee05797a2ad325bfaa3f8e3a
Moon Kang <GYNj7@KTAvb*DSm&dPb%xX9Bjmd3fiq$2@5VA9xUXVT@@p*u9yvB2*Tf45gn^ket>
2026-02-07 00:27:51 -0500

    0.5.1

--- web/flutter_bootstrap.js
+++ web/flutter_bootstrap.js
@@ -38,6 +38,12 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2809247360"
+    serviceWorkerVersion: "3297569369"
+  },
+  config: {
+    // Either of these works; you can keep both:
+    useLocalCanvasKit: true,
+    canvasKitBaseUrl: "canvaskit"
+    // or "canvaskit/" or "/canvaskit/" depending on how you host
   }
 });
